d7e4567572e7d4cc324f595052f4bf9471dd454e
Kalyan Chakravarthy S <kalyanmick@gmail.com>
2015-10-07 02:26:34 +0530

    v1.5
    Implemented Sorting.

--- Mck/assets/scripts/todolist.js
+++ Mck/assets/scripts/todolist.js
@@ -4,6 +4,7 @@ errMsgSave = "The task is be added to the list, altough it is not saved to the l
 errMsgUpdate = "The task is be updated in the list, altough it is not saved to the local storage.";
 errMsgDelete = "This task is deleted from the list, altough this action is not saved to the local storage.";
 errMsgClearStorage = "Clearing local storage is not necessary as we are not storing anything here!";
+errMsgSortOrder = "Task list is sorted, but not saved to local storage.";
 
 var Task = function (data) {
     var self = this;
@@ -14,6 +15,7 @@ var Task = function (data) {
         });
     self.CreatedAt = ko.observable(data ? data.CreatedAt : undefined);
     self.LastUpdatedAt = ko.observable(data ? data.LastUpdatedAt : undefined);
+    self.LastUpdatedDateTime = ko.observable(data ? data.LastUpdatedDateTime : undefined);
     self.CompletionDate = ko.observable(data && data.CompletionDate ? moment(data.CompletionDate).format(preferredDateFormat) : undefined);
     self.IsCompleted = ko.observable(data ? data.IsCompleted : false);
     self.TaskStyle = ko.observable(data && data.IsCompleted ? "CompleteTask" : "IncompleteTask" );
@@ -31,8 +33,12 @@ var vm = function () {
                 var self = this;var savedTaskList;
                 self.MyTask = ko.observable(new Task(null));
                 self.MyTaskList = ko.observableArray([]);
+                self.SortOrder = ko.observable(1);
+                self.SortStyle = ko.observable("k-icon collapse");
                 try{
                 	savedTaskList = localStorage.getItem("TaskList");
+                	savedSortOrder = localStorage.getItem("SortOrder");
+                	savedSortStyle = localStorage.getItem("SortStyle");
                 }
                 catch(ex){
                 	Notify(errMsgLocalStorage, "error");
@@ -43,7 +49,17 @@ var vm = function () {
                 }
                 self.MyTaskList.notifySubscribers();
             }
+
+            if(savedSortOrder){
+            	self.SortOrder(JSON.parse(savedSortOrder));
+            }
             
+			if(savedSortStyle){
+            	self.SortStyle(JSON.parse(savedSortStyle));
+            	self.SortStyle.notifySubscribers();
+            	self.SortStyle.valueHasMutated();
+            }
+
             self.AddTask = function (t) {
                 var NewTask = self.MyTask();
                 var TaskErrors = ko.validation.group(self.MyTask());
@@ -54,6 +70,7 @@ var vm = function () {
                     NewTask.TaskId(len);
                     NewTask.CreatedAt(moment(new Date()).format(preferredDateFormat));
                     NewTask.LastUpdatedAt(moment(new Date()).format(preferredDateFormat));
+                    NewTask.LastUpdatedDateTime(new Date());
                     NewTask.CompletionDate(self.MyTask().CompletionDate() ? moment(self.MyTask().CompletionDate()).format(preferredDateFormat): "");
                     NewTask.IsCompleted(false);
                     self.MyTaskList().push(NewTask);
@@ -89,6 +106,8 @@ var vm = function () {
                 ko.utils.arrayFirst(self.MyTaskList(), function(item) {
                     if(item.TaskId() === t.TaskId()){
                         item.TaskDesc(t.TaskDesc());
+                        item.LastUpdatedAt(moment(new Date()).format(preferredDateFormat));
+                        item.LastUpdatedDateTime(new Date());
                         item.CompletionDate(t.CompletionDate());
                         item.IsCompleted(t.IsCompleted());
                         return item;
@@ -113,6 +132,26 @@ var vm = function () {
            self.ClearStorage = function(){
                 SaveDataToLocalStorage("TaskList", "", errMsgClearStorage);
                 location.reload();
+           }
+
+           self.SortTaskList = function(){
+           	if(self.SortOrder() == 1){
+           		//Ascending
+           		self.MyTaskList().sort(function (l, r) { return l.LastUpdatedDateTime() > r.LastUpdatedDateTime() ? 1 : -1 });
+           		self.SortOrder(2);
+           		self.SortStyle("k-icon expand");
+           	}
+           	else{
+           		//Descending
+           		self.MyTaskList().sort(function (l, r) { return l.LastUpdatedDateTime() < r.LastUpdatedDateTime() ? 1 : -1 });
+           		self.SortOrder(1);
+           		self.SortStyle("k-icon collapse");
+           	}
+           	SaveDataToLocalStorage("TaskList", ko.toJS(self.MyTaskList()), errMsgUpdate);
+           	SaveDataToLocalStorage("SortOrder", self.SortOrder(), errMsgSortOrder);
+           	SaveDataToLocalStorage("SortStyle", self.SortStyle(), errMsgSortOrder);
+			self.MyTaskList.notifySubscribers();
+			self.SortStyle.notifySubscribers();
            }
     };
     var TaskVM = new TaskViewModel();
